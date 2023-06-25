dc1e7b64cb339cb6b202f78862be3de62c1253ca
William Poon <willphk@outlook.com>
2023-06-24 21:49:20 -0400

    Responsive to screen size

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const boardSize = 10;
 const gameBoard = document.getElementById("game-board");
 
-for (let i = 0; i < boardSize; i++) {
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+const boardSizeX = screenWidth / 30;
+const boardSizeY = screenHeight / 30;
+
+for (let i = 0; i < boardSizeY; i++) {
   let row = document.createElement("div");
   row.className = "row";
   row.id = `row-${i}`;
 
-  for (let j = 0; j < boardSize; j++) {
+  for (let j = 0; j < boardSizeX; j++) {
     let cell = document.createElement("input");
     cell.type = "checkbox";
     cell.className = "cell";
