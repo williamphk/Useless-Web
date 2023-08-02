be6d9cdfe01be590a5362cc8e3213358c220c186
William Poon <willphk@outlook.com>
2023-08-02 18:56:14 -0400

    Added Start Button

--- script.js
+++ script.js
@@ -67,91 +67,98 @@ function checkRandomCellAsFood() {
   randomCell.setAttribute("data-food", true);
 }
 
-// Move the snake into the direction 1 checkbox every second
-let moveSnake = setInterval(updatePosition, 100);
+// Start the game
+const startBtn = document.getElementById("start");
+startBtn.addEventListener("click", function () {
+  // Move the snake into the direction 1 checkbox every second
+  let moveSnake = setInterval(updatePosition, 70);
 
-// Update the snake visual position
-function updatePosition() {
-  let lastCell = document.getElementById(
-    `cell-${snakePosition[snakeLength - 1][0]}-${
-      snakePosition[snakeLength - 1][1]
-    }`
-  );
+  // Update the snake visual position
+  function updatePosition() {
+    let lastCell = document.getElementById(
+      `cell-${snakePosition[snakeLength - 1][0]}-${
+        snakePosition[snakeLength - 1][1]
+      }`
+    );
 
-  updateSnakeArray();
+    updateSnakeArray();
 
-  if (gameEnd || snakeAte) {
-  } else {
-    // Uncheck the last cell
-    lastCell.checked = false;
+    if (gameEnd || snakeAte) {
+    } else {
+      // Uncheck the last cell
+      lastCell.checked = false;
+    }
+    snakeAte = false;
+    //console.log(lastCell);
+
+    // Check the new head
+    let forwardCell = document.getElementById(
+      `cell-${snakePosition[0][0]}-${snakePosition[0][1]}`
+    );
+    if (forwardCell) {
+      forwardCell.checked = true;
+    }
+    //console.log(forwardCell);
   }
-  snakeAte = false;
-  //console.log(lastCell);
 
-  // Check the new head
-  let forwardCell = document.getElementById(
-    `cell-${snakePosition[0][0]}-${snakePosition[0][1]}`
-  );
-  forwardCell.checked = true;
-  //console.log(forwardCell);
-}
+  // Event listener for arrow keys
+  document.addEventListener("keydown", function (event) {
+    switch (event.key) {
+      case "ArrowLeft":
+        if (snakeDirection !== "right") snakeDirection = "left";
+        break;
+      case "ArrowRight":
+        if (snakeDirection !== "left") snakeDirection = "right";
+        break;
+      case "ArrowUp":
+        if (snakeDirection !== "down") snakeDirection = "up";
+        break;
+      case "ArrowDown":
+        if (snakeDirection !== "up") snakeDirection = "down";
+        break;
+    }
+  });
 
-// Event listener for arrow keys
-document.addEventListener("keydown", function (event) {
-  switch (event.key) {
-    case "ArrowLeft":
-      if (snakeDirection !== "right") snakeDirection = "left";
-      break;
-    case "ArrowRight":
-      if (snakeDirection !== "left") snakeDirection = "right";
-      break;
-    case "ArrowUp":
-      if (snakeDirection !== "down") snakeDirection = "up";
-      break;
-    case "ArrowDown":
-      if (snakeDirection !== "up") snakeDirection = "down";
-      break;
+  function updateSnakeArray() {
+    const { index, operation } = directionMap[snakeDirection];
+    // Copy the current head position
+    const newHead = [...snakePosition[0]];
+    newHead[index] = operation(newHead[index]); // Update the relevant coordinate
+
+    // Check if the snake is out of bounds
+    if (
+      newHead[0] < 0 ||
+      newHead[0] > screenWidth / 30 ||
+      newHead[1] < 0 ||
+      newHead[1] > screenHeight / 30
+    ) {
+      console.log("Game over");
+      clearInterval(moveSnake);
+      gameEnd = true;
+    }
+
+    // Check if the snake ate the food
+    let snakeHead = document.getElementById(`cell-${newHead[0]}-${newHead[1]}`);
+    if (
+      snakeHead &&
+      snakeHead.checked === true &&
+      snakeHead.getAttribute("data-food") === "true"
+    ) {
+      console.log("Snake ate");
+      snakePosition.unshift(newHead); // Add the new head to the snake body
+      snakeLength++;
+      snakeAte = true;
+      snakeHead.removeAttribute("data-food");
+      checkRandomCellAsFood();
+      return;
+    }
+    if (snakeHead && snakeHead.checked === true) {
+      console.log("Game over");
+      clearInterval(moveSnake);
+      gameEnd = true;
+      return;
+    }
+    snakePosition.unshift(newHead); // Add the new head to the snake body
+    snakePosition.pop();
   }
 });
-
-function updateSnakeArray() {
-  const { index, operation } = directionMap[snakeDirection];
-  // Copy the current head position
-  const newHead = [...snakePosition[0]];
-  newHead[index] = operation(newHead[index]); // Update the relevant coordinate
-
-  // Check if the snake is out of bounds
-  if (
-    newHead[0] < 0 ||
-    newHead[0] > screenWidth / 30 ||
-    newHead[1] < 0 ||
-    newHead[1] > screenHeight / 30
-  ) {
-    console.log("Game over");
-    clearInterval(moveSnake);
-    gameEnd = true;
-  }
-
-  // Check if the snake ate the food
-  let snakeHead = document.getElementById(`cell-${newHead[0]}-${newHead[1]}`);
-  if (
-    snakeHead.checked === true &&
-    snakeHead.getAttribute("data-food") === "true"
-  ) {
-    console.log("Snake ate");
-    snakePosition.unshift(newHead); // Add the new head to the snake body
-    snakeLength++;
-    snakeAte = true;
-    snakeHead.removeAttribute("data-food");
-    checkRandomCellAsFood();
-    return;
-  }
-  // if (snakeHead.checked === true) {
-  //   console.log("Game over");
-  //   clearInterval(moveSnake);
-  //   gameEnd = true;
-  //   return;
-  // }
-  snakePosition.unshift(newHead); // Add the new head to the snake body
-  snakePosition.pop();
-}
